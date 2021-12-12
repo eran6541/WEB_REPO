9e5596a022c52acf7538b117456935e68aba3f73
eran6541 <70238744+eran6541@users.noreply.github.com>
2021-12-11 17:29:45 -0700

    home page edits

---
+++ Final/index.js
@@ -0,0 +1,7 @@
+console.log("javascript working");
+
+document.getElementById("heroimage").addEventListener("click", heroClicked);
+
+function heroClicked(){
+    document.getElementById("main2").innerHTML = "Enjoy my Work!";
+  }
